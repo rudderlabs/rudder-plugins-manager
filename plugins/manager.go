fd58d9c6bbbb2846cbbfc658e7497e8bdbd7b6c5
Dilip Kola <kdilipkola@gmail.com>
2023-03-11 19:29:23 +0530

    feat: add pipeline types

--- plugins/manager.go
+++ plugins/manager.go
@@ -1,6 +1,7 @@
 package plugins
 
 import (
+	"context"
 	"errors"
 
 	"github.com/rudderlabs/rudder-plugins-manager/types"
@@ -10,33 +11,39 @@ import (
  * This file contains the interface that all plugin providers must implement.
  * The interface is used by the plugin manager to load and use plugins.
  */
-type PluginManager struct {
+type BasePluginManager struct {
 	plugins map[string]types.Plugin
 }
 
-type PluginManagerContextKey string
-
-func NewPluginManager() *PluginManager {
-	return &PluginManager{
+func NewBasePluginManager() *BasePluginManager {
+	return &BasePluginManager{
 		plugins: make(map[string]types.Plugin),
 	}
 }
 
-func (m *PluginManager) AddPlugin(plugin types.Plugin) {
+func (m *BasePluginManager) Add(plugin types.Plugin) {
 	m.plugins[plugin.GetName()] = plugin
 }
 
-func (m *PluginManager) AddOrchestrator(plugin types.Plugin) {
-	m.AddPlugin(&OrchestratorPlugin{
+func (m *BasePluginManager) AddOrchestrator(plugin types.Plugin) {
+	m.Add(&OrchestratorPlugin{
 		manager: m,
 		plugin:  plugin,
 	})
 }
 
-func (p *PluginManager) GetPlugin(name string) (types.Plugin, error) {
+func (p *BasePluginManager) Get(name string) (types.Plugin, error) {
 	plugin, ok := p.plugins[name]
 	if !ok {
 		return nil, errors.New("plugin not found")
 	}
 	return plugin, nil
+}
+
+func (p *BasePluginManager) Execute(ctx context.Context, name string, data any) (any, error) {
+	plugin, err := p.Get(name)
+	if err != nil {
+		return nil, err
+	}
+	return plugin.Execute(ctx, data)
 }
